4f56b87ceb53ba282826e726c915af65f2255b02
raad <raadbouks@gmail.com>
2024-06-02 16:18:56 +0000

    update

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://adapting-ram-69.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
